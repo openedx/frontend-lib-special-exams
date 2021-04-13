32e1555ebef3611910ed60aad18fcb5017bde57c
Sagirov Eugeniy <sagirov19@gmail.com>
2021-04-13 17:11:16 +0300

    feat: added userId props for SequenceExamWrapper

--- src/SpecialExamWrapper/Sequence.jsx
+++ src/SpecialExamWrapper/Sequence.jsx
@@ -13,17 +13,19 @@ const SequenceExamWrapper = ({ children, ...props }) => {
   const [examStarted, setExamStarted] = useState(false);
   const [examId, setExamId] = useState();
   const [isLoading, setIsLoading] = useState(true);
-  const { sequence, courseId, loader } = props;
+  const { sequence, courseId, loader, userId } = props;
 
   const getExamData = async () => {
-    const examUrl = new URL(`${getConfig().LMS_BASE_URL}/api/edx_proctoring/v1/proctored_exam/exam/course_id/${courseId}/content_id/${sequence.id}`);
+    const examUrl = new URL(
+      `${getConfig().LMS_BASE_URL}/api/edx_proctoring/v1/proctored_exam/exam/course_id/${courseId}/content_id/${sequence.id}`
+    );
     const { data } = await getAuthenticatedHttpClient().get(examUrl.href);
     setExamId(data.id);
   };
 
   const getAtemptData = async() => {
     // TODO: find where to get user_id
-    const url = new URL(`${getConfig().LMS_BASE_URL}/api/edx_proctoring/v1/proctored_exam/active_exams_for_user?user_id=3&course_id=${encodeURIComponent(courseId)}`);
+    const url = new URL(`${getConfig().LMS_BASE_URL}/api/edx_proctoring/v1/proctored_exam/active_exams_for_user?user_id=${userId}&course_id=${encodeURIComponent(courseId)}`);
     const { data } = await getAuthenticatedHttpClient().get(url.href);
     if (Object.keys(data).length > 0) {
       const examData = data[0];
@@ -69,6 +71,7 @@ SequenceExamWrapper.propTypes = {
   sequence: PropTypes.shape({
     isTimeLimited: PropTypes.bool,
   }).isRequired,
+  userId: PropTypes.number.isRequired,
 };
 
 // eslint-disable-next-line import/prefer-default-export
