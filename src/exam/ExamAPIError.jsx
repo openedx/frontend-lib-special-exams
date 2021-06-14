ccc7bf9869950a45660df2c7e447e6f08b8115ed
Viktor Rusakov <vrusakov66@gmail.com>
2021-06-14 19:03:10 +0300

    feat: improve handling of API errors

--- src/exam/ExamAPIError.jsx
+++ src/exam/ExamAPIError.jsx
@@ -1,38 +1,66 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
 import { Alert, Icon } from '@edx/paragon';
 import { Info } from '@edx/paragon/icons';
 import { FormattedMessage } from '@edx/frontend-platform/i18n';
+import ExamStateContext from '../context';
 
-// Fixme: Replace with store state data once exam settings are implemented
-const platformName = 'edX';
-const contactUs = 'https://courses.edx.org/support/contact_us';
+export default function ExamAPIError() {
+  const state = useContext(ExamStateContext);
+  const { apiErrorMsg, proctoringSettings } = state;
+  const { contact_us: contactUs, platform_name: platformName } = proctoringSettings;
+  const [showError, setShowError] = useState(false);
 
-export default function ExamAPIError({ details }) {
+  const renderHeading = () => {
+    if (contactUs && platformName) {
+      return (
+        <FormattedMessage
+          id="exam.apiError.text1"
+          defaultMessage={
+            'A system error has occurred with your exam. '
+              + 'Please reach out to {supportLink} for assistance, '
+              + 'and return to the exam once you receive further instructions.'
+          }
+          values={{ supportLink: <a href={contactUs} target="_blank" rel="noopener noreferrer">{platformName} Support</a> }}
+        />
+      );
+    }
+
+    return (
+      <FormattedMessage
+        id="exam.apiError.text2"
+        defaultMessage="A system error has occurred with your exam. Please reach out to support for assistance."
+      />
+    );
+  };
+
   return (
     <Alert variant="danger">
       <Icon src={Info} className="alert-icon" />
       <Alert.Heading>
-        <FormattedMessage
-          id="exam.apiError.text"
-          defaultMessage={
-            'A system error has occurred with your exam. '
-              + 'Please reach out to {support_link} for assistance, '
-              + 'and return to the exam once you receive further instructions.'
-          }
-          values={{ support_link: <a href={contactUs} target="_blank" rel="noopener noreferrer">{platformName} Support</a> }}
-        />
+        {renderHeading()}
       </Alert.Heading>
       <p>
         <FormattedMessage
           id="exam.apiError.details"
           defaultMessage="Details"
-        />: {details}
+        />:
+        <span className="pl-2">
+          <Alert.Link onClick={() => setShowError(!showError)}>
+            {showError ? (
+              <FormattedMessage
+                id="exam.apiError.showLink"
+                defaultMessage="Hide"
+              />
+            ) : (
+              <FormattedMessage
+                id="exam.apiError.hideLink"
+                defaultMessage="Show"
+              />
+            )}
+          </Alert.Link>
+        </span>
       </p>
+      {showError && <p>{apiErrorMsg}</p>}
     </Alert>
   );
 }
-
-ExamAPIError.propTypes = {
-  details: PropTypes.string.isRequired,
-};
