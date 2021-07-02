7a4d55d1c6a1cf567b574309d5aaf8403db2ac34
Viktor Rusakov <vrusakov66@gmail.com>
2021-07-02 15:03:53 +0300

    fix: remove review exam text on submitted page

--- src/instructions/timed_exam/SubmittedTimedExamInstructions.jsx
+++ src/instructions/timed_exam/SubmittedTimedExamInstructions.jsx
@@ -6,30 +6,21 @@ const SubmittedTimedExamInstructions = () => {
   const state = useContext(ExamStateContext);
 
   return (
-    <>
-      <h3 className="h3" data-testid="exam.submittedExamInstructions.title">
-        {state.timeIsOver
-          ? (
-            <FormattedMessage
-              id="exam.submittedExamInstructions.overtimeTitle"
-              defaultMessage="The time allotted for this exam has expired. Your exam has been submitted and any work you completed will be graded."
-            />
-          )
-          : (
-            <FormattedMessage
-              id="exam.submittedExamInstructions.title"
-              defaultMessage="You have submitted your timed exam."
-            />
-          )}
-      </h3>
-      <p>
-        <FormattedMessage
-          id="exam.submittedExamInstructions.text"
-          defaultMessage={'After the due date has passed, you can review the exam,'
-                + ' but you cannot change your answers.'}
-        />
-      </p>
-    </>
+    <h3 className="h3" data-testid="exam.submittedExamInstructions.title">
+      {state.timeIsOver
+        ? (
+          <FormattedMessage
+            id="exam.submittedExamInstructions.overtimeTitle"
+            defaultMessage="The time allotted for this exam has expired. Your exam has been submitted and any work you completed will be graded."
+          />
+        )
+        : (
+          <FormattedMessage
+            id="exam.submittedExamInstructions.title"
+            defaultMessage="You have submitted your timed exam."
+          />
+        )}
+    </h3>
   );
 };
 
