9bcfe1c528cdedbdd697bc6353f5e3c6525161f3
Simon Chen <schen@edx-c02fw0guml85.lan>
2021-09-23 14:34:16 -0400

    build: integrate commitlint
        Integrate commitlint into the repository pre-commit check so commit messages can trigger semantic-release properly

---
+++ commitlint.config.js
@@ -0,0 +1,33 @@
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    'body-max-line-length': [2, 'always', Infinity],
+    'subject-case': [
+      2,
+      'never',
+      [],
+    ],
+  },
+  /*
+   * Functions that return true if commitlint should ignore the given message.
+   */
+  ignores: [(commit) => commit === ''],
+  /*
+   * Whether commitlint uses the default ignore rules.
+   */
+  defaultIgnores: true,
+  /*
+   * Custom URL to show upon failure
+   */
+  helpUrl:
+    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+};
+
+module.exports = Configuration;
